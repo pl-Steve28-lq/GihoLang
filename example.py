fc27a946ac62423b86c6ac54f1951f1875b39428
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-03-01 22:50:41 +0900

    힘들었다

---
+++ example.py
@@ -0,0 +1,4 @@
+from giho import Giho
+
+g = Giho()
+print(g.executePath('./example/factorial.gh'))
---
+++ giho.py
@@ -0,0 +1,106 @@
+class Giho:
+  def __init__(self, size=256):
+    self.stack = []
+    self.result = []
+  
+  def executePath(self, path):
+    with open(path, encoding='utf-8') as f:
+      code = f.read()
+      return self.execute(code)
+  
+  def execute(self, code):
+    self.__init__()
+    code = self.filter(self.stripLine(code))
+    length = len(code)
+    idx = 0
+    loop = self.findLoop(code)
+    #print(code)
+    while idx < length:
+      ch = code[idx]
+      #print(self.stack, self.result)
+
+      if ch == '&': self.stack.append(1)
+      if ch == '.' and self.stack: self.result.append(self.stack.pop())
+      if ch == ',' and self.stack: self.stack.append(self.stack.pop() + self.stack.pop())
+      if ch == '!' and self.result: self.result.pop()
+      if ch == '?': self.stack.append(-1)
+      if ch == '$' and self.result:
+        self.stack.append(self.result.pop())
+      if ch == '#':
+        print(self.result[-1])
+      if ch == '^':
+        self.stack = list(reversed(self.stack))
+      if ch == '`' and self.stack:
+        ind = self.result.pop()-1
+        self.result.append(self.stack[ind])
+      if ch == '\n':
+        self.stack.append(int(input()))
+      
+      if ch == '(' and self.stack[-1] <= 0:
+        idx = loop[idx]-1
+      if ch == ')' and self.stack[-1] > 0:
+        idx = loop[idx]-1
+      if ch in '\'"':
+        l = loop[idx]
+        if (
+          (l[1] == 'left' and self.stack[-1] <= 0) or
+          (l[1] == 'right' and self.stack[-1] > 0)
+        ): idx = loop[idx]-1
+      
+      idx += 1
+    return [self.stack, self.result]
+  
+  def findLoop(self, code):
+    st = []
+    res = {}
+    for i in range(len(code)):
+      ch = code[i]
+      if ch == '(': st.append([i, ch])
+      if not st: continue
+      s = st[-1]
+      if ch == ')':
+        if s[1] == '(':
+          p = st.pop()
+          res[p[0]] = i
+          res[i] = p[0]
+      if ch in '\'"':
+        if s[1] == ch:
+          p = st.pop()
+          res[p[0]] = [i, 'left']
+          res[i] = [p[0], 'right']
+        else:
+          st.append([i, ch])
+    return res     
+  
+  def filter(self, code):
+    res = ''
+    code = self.replAll(code, {
+      '   ': '^', '  ': '&', ' .': '#', ' ,': '$', ' !': '`', '\n ': ''
+    })
+    for ch in code:
+      if ch in '.,!?()\'"\n^#$`&':
+        res += ch
+    return self.replAll(res, {
+      '()': '', '""': '', '\'\'': ''
+    })
+
+  def replAll(self, targ, info):
+    for i in info:
+      targ = targ.replace(i, info[i])
+    return targ
+  
+  def stripLine(self, targ):
+    while True:
+      if targ[0] != '\n': break
+      targ = targ[1:]
+    while True:
+      if targ[-1] != '\n': break
+      targ = targ[:-1]
+    return targ
+
+  def simplify(self, code):
+    filt = { '   ': '^', '  ': '&', ' .': '#', ' ,': '$', ' !': '`', '\n ': ''}
+    code = self.filter(code)
+    for i in filt:
+      code = code.replace(filt[i], i)
+    return code
